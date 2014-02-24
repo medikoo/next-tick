64c253b46b1e6c7f12be09f89b92c947370d5790
Mariusz Nowak <mariusz@medikoo.com>
2014-02-24 16:42:27 +0100

    Microtask resolution via MutationObserver. #1

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 'use strict';
 
-var callable = require('es5-ext/object/valid-callable');
+var callable = require('es5-ext/object/valid-callable')
+
+  , byObserver;
+
+byObserver = function (Observer) {
+	var el = document.createElement('div'), queue;
+	new Observer(function () {
+		var data;
+		if (!queue) return;
+		data = queue;
+		queue = null;
+		if (typeof data === 'function') {
+			data();
+			return;
+		}
+		data.forEach(function (fn) { fn(); });
+	}).observe(el, { attributes: true });
+	return function (fn) {
+		callable(fn);
+		if (queue) {
+			if (typeof queue === 'function') queue = [queue, fn];
+			else queue.push(fn);
+			return;
+		}
+		queue = fn;
+		el.setAttribute('x', 'y');
+	};
+};
 
 module.exports = (function () {
 	// Node.js
 	if ((typeof process !== 'undefined') && process &&
 			(typeof process.nextTick === 'function')) {
 		return process.nextTick;
+	}
+
+	// MutationObserver
+	if ((typeof document === 'object') && document) {
+		if (typeof MutationObserver === 'function') {
+			return byObserver(MutationObserver);
+		}
+		if (typeof WebKitMutationObserver === 'function') {
+			return byObserver(WebKitMutationObserver);
+		}
 	}
 
 	// W3C Draft
