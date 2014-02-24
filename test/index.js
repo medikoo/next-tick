bee278faa8706fe7ebdf36a6cc48801cf0ea86b9
Mariusz Nowak <mariusz@medikoo.com>
2014-02-24 16:42:38 +0100

    Add tests for serial calls

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 
 module.exports = function (t, a, d) {
 	var invoked;
+
 	a(t(function () {
 		a(arguments.length, 0, "Arguments");
 		invoked = true;
@@ -9,6 +10,13 @@ module.exports = function (t, a, d) {
 	a(invoked, undefined, "Is not run immediately");
 	setTimeout(function () {
 		a(invoked, true, "Run in next tick");
-		d();
+		invoked = [];
+		t(function () { invoked.push(0); });
+		t(function () { invoked.push(1); });
+		t(function () { invoked.push(2); });
+		setTimeout(function () {
+			a.deep(invoked, [0, 1, 2], "Serial");
+			d();
+		}, 10);
 	}, 10);
 };
